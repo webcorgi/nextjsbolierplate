1d3ebff4249f8eee325de575e6c1d9ed7be7b7d0
DAEHYUN KIM <32122611+webcogy@users.noreply.github.com>
2024-03-11 11:04:15 +0900

    first

---
+++ pages/_app.tsx
@@ -0,0 +1,18 @@
+import "../styles/globals.css";
+
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import type { AppProps } from "next/app";
+
+import { RecoilRoot } from "recoil";
+
+export default function App({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </QueryClientProvider>
+  );
+}
---
+++ styles/fonts.tsx
@@ -0,0 +1,803 @@
+import { css } from "styled-components";
+
+interface IFont {
+  [name: string]: any
+}
+
+export const fonts: IFont = {
+  T9Heavy: css`
+    font-family: "NotoSansKR-Black";
+    font-size: 9px;
+    // line-height: 13px;
+  `,
+  T9Bold: css`
+    font-family: "NotoSansKR-Bold";
+    font-size: 9px;
+    // line-height: 13px;
+  `,
+  T9Medium: css`
+    font-family: "NotoSansKR-Medium";
+    font-size: 9px;
+    font-weight: 500;
+    // line-height: 13px;
+  `,
+  T9Regular: css`
+    font-family: "NotoSansKR-Regular";
+    font-size: 9px;
+    // line-height: 13px;
+  `,
+  T9Light: css`
+    font-family: "NotoSansKR-Light";
+    font-size: 9px;
+    // line-height: 13px;
+  `,
+
+  T10Heavy: css`
+    font-family: "NotoSansKR-Black";
+    font-size: 10px;
+    line-height: 13px;
+  `,
+  T10Bold: css`
+    font-family: "NotoSansKR-Bold";
+    font-size: 10px;
+    line-height: 13px;
+  `,
+  T10Medium: css`
+    font-family: "NotoSansKR-Medium";
+    font-size: 10px;
+    line-height: 13px;
+  `,
+  T10Regular: css`
+    font-family: "NotoSansKR-Regular";
+    font-size: 10px;
+    line-height: 13px;
+  `,
+  T10Light: css`
+    font-family: "NotoSansKR-Light";
+    font-size: 10px;
+    line-height: 13px;
+  `,
+
+  T11Heavy: css`
+    font-family: "NotoSansKR-Black";
+    font-size: 11px;
+    line-height: 13px;
+  `,
+  T11Bold: css`
+    font-family: "NotoSansKR-Bold";
+    font-size: 11px;
+    line-height: 13px;
+  `,
+  T11Medium: css`
+    font-family: "NotoSansKR-Medium";
+    font-size: 11px;
+    line-height: 13px;
+  `,
+  T11Regular: css`
+    font-family: "NotoSansKR-Regular";
+    font-size: 11px;
+    line-height: 13px;
+  `,
+  T11Light: css`
+    font-family: "NotoSansKR-Light";
+    font-size: 11px;
+    line-height: 13px;
+  `,
+  T12Heavy: css`
+    font-family: "NotoSansKR-Black";
+    font-size: 12px;
+    line-height: 16px;
+  `,
+  T12Bold: css`
+    font-family: "NotoSansKR-Bold";
+    font-size: 12px;
+    line-height: 16px;
+  `,
+  T12Medium: css`
+    font-family: "NotoSansKR-Medium";
+    font-size: 12px;
+    line-height: 16px;
+  `,
+  T12Regular: css`
+    font-family: "NotoSansKR-Regular";
+    font-size: 12px;
+    line-height: 16px;
+  `,
+  T12Light: css`
+    font-family: "NotoSansKR-Light";
+    font-size: 12px;
+    line-height: 16px;
+  `,
+  T13Heavy: css`
+    font-family: "NotoSansKR-Black";
+    font-size: 13px;
+    // line-height: 15px;
+  `,
+  T13Bold: css`
+    font-family: "NotoSansKR-Bold";
+    font-size: 13px;
+    // line-height: 15px;
+  `,
+  T13Medium: css`
+    font-family: "NotoSansKR-Medium";
+    font-size: 13px;
+    // line-height: 15px;
+  `,
+  T13Regular: css`
+    font-family: "NotoSansKR-Regular";
+    font-size: 13px;
+    // line-height: 15px;
+  `,
+  T13Light: css`
+    font-family: "NotoSansKR-Light";
+    font-size: 13px;
+    // line-height: 15px;
+  `,
+  T14Heavy: css`
+    font-family: "NotoSansKR-Black";
+    font-size: 14px;
+    line-height: 16px;
+  `,
+  T14Bold: css`
+    font-family: "NotoSansKR-Bold";
+    font-size: 14px;
+    line-height: 16px;
+  `,
+  T14Medium: css`
+    font-family: "NotoSansKR-Medium";
+    font-size: 14px;
+    line-height: 16px;
+  `,
+  T14Regular: css`
+    font-family: "NotoSansKR-Regular";
+    font-size: 14px;
+    line-height: 16px;
+  `,
+  T14Light: css`
+    font-family: "NotoSansKR-Light";
+    font-size: 14px;
+    line-height: 16px;
+  `,
+  T15Heavy: css`
+    font-family: "NotoSansKR-Black";
+    font-size: 15px;
+  `,
+  T15Bold: css`
+    font-family: "NotoSansKR-Bold";
+    font-size: 15px;
+  `,
+  T15Medium: css`
+    font-family: "NotoSansKR-Medium";
+    font-size: 15px;
+  `,
+  T15Regular: css`
+    font-family: "NotoSansKR-Regular";
+    font-size: 15px;
+  `,
+  T15Light: css`
+    font-family: "NotoSansKR-Light";
+    font-size: 15px;
+  `,
+  T16Heavy: css`
+    font-family: "NotoSansKR-Black";
+    font-size: 16px;
+    line-height: 18px;
+  `,
+  T16Bold: css`
+    font-family: "NotoSansKR-Bold";
+    font-size: 16px;
+    line-height: 18px;
+  `,
+  T16Medium: css`
+    font-family: "NotoSansKR-Medium";
+    font-size: 16px;
+    line-height: 18px;
+  `,
+  T16Regular: css`
+    font-family: "NotoSansKR-Regular";
+    font-size: 16px;
+    line-height: 18px;
+  `,
+  T16Light: css`
+    font-family: "NotoSansKR-Light";
+    font-size: 16px;
+    line-height: 18px;
+  `,
+  T17Heavy: css`
+    font-family: "NotoSansKR-Black";
+    font-size: 17px;
+    line-height: 21px;
+  `,
+  T17Bold: css`
+    font-family: "NotoSansKR-Bold";
+    font-size: 17px;
+    line-height: 21px;
+  `,
+  T17Medium: css`
+    font-family: "NotoSansKR-Medium";
+    font-size: 17px;
+    line-height: 21px;
+  `,
+  T17Regular: css`
+    font-family: "NotoSansKR-Regular";
+    font-size: 17px;
+    line-height: 21px;
+  `,
+  T17Light: css`
+    font-family: "NotoSansKR-Light";
+    font-size: 17px;
+    line-height: 21px;
+  `,
+  T18Heavy: css`
+    font-family: "NotoSansKR-Black";
+    font-size: 18px;
+    line-height: 20px;
+  `,
+  T18Bold: css`
+    font-family: "NotoSansKR-Bold";
+    font-size: 18px;
+    line-height: 20px;
+  `,
+  T18Medium: css`
+    font-family: "NotoSansKR-Medium";
+    font-size: 18px;
+    line-height: 20px;
+  `,
+  T18Regular: css`
+    font-family: "NotoSansKR-Regular";
+    font-size: 18px;
+    line-height: 20px;
+  `,
+  T18Light: css`
+    font-family: "NotoSansKR-Light";
+    font-size: 18px;
+    line-height: 20px;
+  `,
+  T19Heavy: css`
+    font-family: "NotoSansKR-Black";
+    font-size: 19px;
+    line-height: 21px;
+  `,
+  T19Bold: css`
+    font-family: "NotoSansKR-Bold";
+    font-size: 19px;
+    line-height: 21px;
+  `,
+  T19Medium: css`
+    font-family: "NotoSansKR-Medium";
+    font-size: 19px;
+    line-height: 21px;
+  `,
+  T19Regular: css`
+    font-family: "NotoSansKR-Regular";
+    font-size: 19px;
+    line-height: 21px;
+  `,
+  T19Light: css`
+    font-family: "NotoSansKR-Light";
+    font-size: 19px;
+    line-height: 21px;
+  `,
+  T20Heavy: css`
+    font-family: "NotoSansKR-Black";
+    font-size: 20px;
+    line-height: 22px;
+  `,
+  T20Bold: css`
+    font-family: "NotoSansKR-Bold";
+    font-size: 20px;
+    line-height: 22px;
+  `,
+  T20Medium: css`
+    font-family: "NotoSansKR-Medium";
+    font-size: 20px;
+    line-height: 22px;
+  `,
+  T20Regular: css`
+    font-family: "NotoSansKR-Regular";
+    font-size: 20px;
+    line-height: 22px;
+  `,
+  T20Light: css`
+    font-family: "NotoSansKR-Light";
+    font-size: 20px;
+    line-height: 22px;
+  `,
+  T21Heavy: css`
+    font-family: "NotoSansKR-Black";
+    font-size: 21px;
+    line-height: 23px;
+  `,
+  T21Bold: css`
+    font-family: "NotoSansKR-Bold";
+    font-size: 21px;
+    line-height: 23px;
+  `,
+  T21Medium: css`
+    font-family: "NotoSansKR-Medium";
+    font-size: 21px;
+    line-height: 23px;
+  `,
+  T21Regular: css`
+    font-family: "NotoSansKR-Regular";
+    font-size: 21px;
+    line-height: 23px;
+  `,
+  T21Light: css`
+    font-family: "NotoSansKR-Light";
+    font-size: 21px;
+    line-height: 23px;
+  `,
+  T22Heavy: css`
+    font-family: "NotoSansKR-Black";
+    font-size: 22px;
+  `,
+  T22Bold: css`
+    font-family: "NotoSansKR-Bold";
+    font-size: 22px;
+  `,
+  T22Medium: css`
+    font-family: "NotoSansKR-Medium";
+    font-size: 22px;
+  `,
+  T22Regular: css`
+    font-family: "NotoSansKR-Regular";
+    font-size: 22px;
+  `,
+  T22Light: css`
+    font-family: "NotoSansKR-Light";
+    font-size: 22px;
+  `,
+  T23Heavy: css`
+    font-family: "NotoSansKR-Black";
+    font-size: 23px;
+    line-height: 25px;
+  `,
+  T23Bold: css`
+    font-family: "NotoSansKR-Bold";
+    font-size: 23px;
+    line-height: 25px;
+  `,
+  T23Medium: css`
+    font-family: "NotoSansKR-Medium";
+    font-size: 23px;
+    line-height: 25px;
+  `,
+  T23Regular: css`
+    font-family: "NotoSansKR-Regular";
+    font-size: 23px;
+    line-height: 25px;
+  `,
+  T23Light: css`
+    font-family: "NotoSansKR-Light";
+    font-size: 23px;
+    line-height: 25px;
+  `,
+  T24Heavy: css`
+    font-family: "NotoSansKR-Black";
+    font-size: 24px;
+    line-height: 26px;
+  `,
+  T24Bold: css`
+    font-family: "NotoSansKR-Bold";
+    font-size: 24px;
+    line-height: 26px;
+  `,
+  T24Medium: css`
+    font-family: "NotoSansKR-Medium";
+    font-size: 24px;
+    line-height: 26px;
+  `,
+  T24Regular: css`
+    font-family: "NotoSansKR-Regular";
+    font-size: 24px;
+    line-height: 26px;
+  `,
+  T24Light: css`
+    font-family: "NotoSansKR-Light";
+    font-size: 24px;
+    line-height: 26px;
+  `,
+  T25Heavy: css`
+    font-family: "NotoSansKR-Black";
+    font-size: 25px;
+    line-height: 29px;
+  `,
+  T25Bold: css`
+    font-family: "NotoSansKR-Bold";
+    font-size: 25px;
+    line-height: 29px;
+  `,
+  T25Medium: css`
+    font-family: "NotoSansKR-Medium";
+    font-size: 25px;
+    line-height: 29px;
+  `,
+  T25Regular: css`
+    font-family: "NotoSansKR-Regular";
+    font-size: 25px;
+    line-height: 29px;
+  `,
+  T25Light: css`
+    font-family: "NotoSansKR-Light";
+    font-size: 25px;
+    line-height: 29px;
+  `,
+  T26Heavy: css`
+    font-family: "NotoSansKR-Black";
+    font-size: 26px;
+    line-height: 30px;
+  `,
+  T26Bold: css`
+    font-family: "NotoSansKR-Bold";
+    font-size: 26px;
+    line-height: 30px;
+  `,
+  T26Medium: css`
+    font-family: "NotoSansKR-Medium";
+    font-size: 26px;
+    line-height: 30px;
+  `,
+  T26Regular: css`
+    font-family: "NotoSansKR-Regular";
+    font-size: 26px;
+    line-height: 30px;
+  `,
+  T26Light: css`
+    font-family: "NotoSansKR-Light";
+    font-size: 26px;
+    line-height: 30px;
+  `,
+  T27Heavy: css`
+    font-family: "NotoSansKR-Black";
+    font-size: 27px;
+    line-height: 31px;
+  `,
+  T27Bold: css`
+    font-family: "NotoSansKR-Bold";
+    font-size: 27px;
+    line-height: 31px;
+  `,
+  T27Medium: css`
+    font-family: "NotoSansKR-Medium";
+    font-size: 27px;
+    line-height: 31px;
+  `,
+  T27Regular: css`
+    font-family: "NotoSansKR-Regular";
+    font-size: 27px;
+    line-height: 31px;
+  `,
+  T27Light: css`
+    font-family: "NotoSansKR-Light";
+    font-size: 27px;
+    line-height: 31px;
+  `,
+  T28Heavy: css`
+    font-family: "NotoSansKR-Black";
+    font-size: 28px;
+    line-height: 32px;
+  `,
+  T28Bold: css`
+    font-family: "NotoSansKR-Bold";
+    font-size: 28px;
+    line-height: 32px;
+  `,
+  T28Medium: css`
+    font-family: "NotoSansKR-Medium";
+    font-size: 28px;
+    line-height: 32px;
+  `,
+  T28Regular: css`
+    font-family: "NotoSansKR-Regular";
+    font-size: 28px;
+    line-height: 32px;
+  `,
+  T28Light: css`
+    font-family: "NotoSansKR-Light";
+    font-size: 28px;
+    line-height: 32px;
+  `,
+  T29Heavy: css`
+    font-family: "NotoSansKR-Black";
+    font-size: 29px;
+    line-height: 33px;
+  `,
+  T29Bold: css`
+    font-family: "NotoSansKR-Bold";
+    font-size: 29px;
+    line-height: 33px;
+  `,
+  T29Medium: css`
+    font-family: "NotoSansKR-Medium";
+    font-size: 29px;
+    line-height: 33px;
+  `,
+  T29Regular: css`
+    font-family: "NotoSansKR-Regular";
+    font-size: 29px;
+    line-height: 33px;
+  `,
+  T29Light: css`
+    font-family: "NotoSansKR-Light";
+    font-size: 29px;
+    line-height: 33px;
+  `,
+  T30Heavy: css`
+    font-family: "NotoSansKR-Black";
+    font-size: 30px;
+    line-height: 34px;
+  `,
+  T30Bold: css`
+    font-family: "NotoSansKR-Bold";
+    font-size: 30px;
+    line-height: 34px;
+  `,
+  T30Medium: css`
+    font-family: "NotoSansKR-Medium";
+    font-size: 30px;
+    line-height: 34px;
+  `,
+  T30Regular: css`
+    font-family: "NotoSansKR-Regular";
+    font-size: 30px;
+    line-height: 34px;
+  `,
+  T30Light: css`
+    font-family: "NotoSansKR-Light";
+    font-size: 30px;
+    line-height: 34px;
+  `,
+  T31Heavy: css`
+    font-family: "NotoSansKR-Black";
+    font-size: 31px;
+    line-height: 35px;
+  `,
+  T31Bold: css`
+    font-family: "NotoSansKR-Bold";
+    font-size: 31px;
+    line-height: 35px;
+  `,
+  T31Medium: css`
+    font-family: "NotoSansKR-Medium";
+    font-size: 31px;
+    line-height: 35px;
+  `,
+  T31Regular: css`
+    font-family: "NotoSansKR-Regular";
+    font-size: 31px;
+    line-height: 35px;
+  `,
+  T31Light: css`
+    font-family: "NotoSansKR-Light";
+    font-size: 31px;
+    line-height: 35px;
+  `,
+  T32Heavy: css`
+    font-family: "NotoSansKR-Black";
+    font-size: 32px;
+    line-height: 38px;
+  `,
+  T32Bold: css`
+    font-family: "NotoSansKR-Bold";
+    font-size: 32px;
+    line-height: 38px;
+  `,
+  T32Medium: css`
+    font-family: "NotoSansKR-Medium";
+    font-size: 32px;
+    line-height: 38px;
+  `,
+  T32Regular: css`
+    font-family: "NotoSansKR-Regular";
+    font-size: 32px;
+    line-height: 38px;
+  `,
+  T32Light: css`
+    font-family: "NotoSansKR-Light";
+    font-size: 32px;
+    line-height: 38px;
+  `,
+  T33Heavy: css`
+    font-family: "NotoSansKR-Black";
+    font-size: 33px;
+    line-height: 37px;
+  `,
+  T33Bold: css`
+    font-family: "NotoSansKR-Bold";
+    font-size: 33px;
+    line-height: 37px;
+  `,
+  T33Medium: css`
+    font-family: "NotoSansKR-Medium";
+    font-size: 33px;
+    line-height: 37px;
+  `,
+  T33Regular: css`
+    font-family: "NotoSansKR-Regular";
+    font-size: 33px;
+    line-height: 37px;
+  `,
+  T33Light: css`
+    font-family: "NotoSansKR-Light";
+    font-size: 33px;
+    line-height: 37px;
+  `,
+  T34Heavy: css`
+    font-family: "NotoSansKR-Black";
+    font-size: 34px;
+    line-height: 38px;
+  `,
+  T34Bold: css`
+    font-family: "NotoSansKR-Bold";
+    font-size: 34px;
+    line-height: 38px;
+  `,
+  T34Medium: css`
+    font-family: "NotoSansKR-Medium";
+    font-size: 34px;
+    line-height: 38px;
+  `,
+  T34Regular: css`
+    font-family: "NotoSansKR-Regular";
+    font-size: 34px;
+    line-height: 38px;
+  `,
+  T34Light: css`
+    font-family: "NotoSansKR-Light";
+    font-size: 34px;
+    line-height: 38px;
+  `,
+  T35Heavy: css`
+    font-family: "NotoSansKR-Black";
+    font-size: 35px;
+    line-height: 41px;
+  `,
+  T35Bold: css`
+    font-family: "NotoSansKR-Bold";
+    font-size: 35px;
+    line-height: 41px;
+  `,
+  T35Medium: css`
+    font-family: "NotoSansKR-Medium";
+    font-size: 35px;
+    line-height: 41px;
+  `,
+  T35Regular: css`
+    font-family: "NotoSansKR-Regular";
+    font-size: 35px;
+    line-height: 41px;
+  `,
+  T35Light: css`
+    font-family: "NotoSansKR-Light";
+    font-size: 35px;
+    line-height: 41px;
+  `,
+
+  T36Heavy: css`
+    font-family: "NotoSansKR-Black";
+    font-size: 36px;
+    line-height: 42px;
+  `,
+  T36Bold: css`
+    font-family: "NotoSansKR-Bold";
+    font-size: 36px;
+    line-height: 42px;
+  `,
+  T36Medium: css`
+    font-family: "NotoSansKR-Medium";
+    font-size: 36px;
+    line-height: 42px;
+  `,
+  T36Regular: css`
+    font-family: "NotoSansKR-Regular";
+    font-size: 36px;
+    line-height: 42px;
+  `,
+  T36Light: css`
+    font-family: "NotoSansKR-Light";
+    font-size: 36px;
+    line-height: 42px;
+  `,
+
+  T37Heavy: css`
+    font-family: "NotoSansKR-Black";
+    font-size: 37px;
+    line-height: 43px;
+  `,
+  T37Bold: css`
+    font-family: "NotoSansKR-Bold";
+    font-size: 37px;
+    line-height: 43px;
+  `,
+  T37Medium: css`
+    font-family: "NotoSansKR-Medium";
+    font-size: 37px;
+    line-height: 43px;
+  `,
+  T37Regular: css`
+    font-family: "NotoSansKR-Regular";
+    font-size: 37px;
+    line-height: 43px;
+  `,
+  T37Light: css`
+    font-family: "NotoSansKR-Light";
+    font-size: 37px;
+    line-height: 43px;
+  `,
+
+  T40Heavy: css`
+    font-family: "NotoSansKR-Black";
+    font-size: 40px;
+    line-height: 46px;
+  `,
+  T50Heavy: css`
+    font-family: "NotoSansKR-Black";
+    font-size: 50px;
+    line-height: 56px;
+  `,
+  T60Heavy: css`
+    font-family: "NotoSansKR-Black";
+    font-size: 60px;
+    line-height: 66px;
+  `,
+  T70Heavy: css`
+    font-family: "NotoSansKR-Black";
+    font-size: 70px;
+    line-height: 76px;
+  `,
+  T80Heavy: css`
+    font-family: "NotoSansKR-Black";
+    font-size: 80px;
+    line-height: 86px;
+  `,
+  T90Heavy: css`
+    font-family: "NotoSansKR-Black";
+    font-size: 90px;
+    line-height: 96px;
+  `,
+  T100Heavy: css`
+    font-family: "NotoSansKR-Black";
+    font-size: 100px;
+    line-height: 106px;
+  `,
+
+  T17RotoBold: css`
+    font-family: "RobotoSlab-Bold";
+    font-size: 17px;
+  `,
+  T20RotoBold: css`
+    font-family: "RobotoSlab-Bold";
+    font-size: 20px;
+  `,
+  T27RotoBold: css`
+    font-family: "RobotoSlab-Bold";
+    font-size: 27px;
+  `,
+  T30RotoBold: css`
+    font-family: "RobotoSlab-Bold";
+    font-size: 30px;
+  `,
+
+  T17RotoHeavy: css`
+    font-family: "RobotoSlab-Black";
+    font-size: 17px;
+  `,
+  T20RotoHeavy: css`
+    font-family: "RobotoSlab-Black";
+    font-size: 20px;
+  `,
+  T27RotoHeavy: css`
+    font-family: "RobotoSlab-Black";
+    font-size: 27px;
+  `,
+  T30RotoHeavy: css`
+    font-family: "RobotoSlab-Black";
+    font-size: 30px;
+  `,
+  T40RotoHeavy: css`
+    font-family: "RobotoSlab-Black";
+    font-size: 40px;
+    line-height: 44px;
+  `
+};
